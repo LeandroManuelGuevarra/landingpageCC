0ddf448b9404ba5e58742384a77c4d0b3d4866dc
Manuel0420 <leandro.guevarra0420@gmail.com>
2021-05-25 20:53:07 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from '../components/Navigation';
+import Units from '../components/Units';
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+      </Head>
+      <Navigation />
+      <Units />
+    </React.Fragment>    
+  )
+}
